d984daf8ee33735effd292d0e4abc949381fc20e
Leandro Pereira de Almeida Junior <89665500+leandro-jr@users.noreply.github.com>
2022-02-24 17:38:06 -0300

    Update settings_bkp.py
    Removed sensitive information

--- project_final/fulltank/fulltank/settings_bkp.py
+++ project_final/fulltank/fulltank/settings_bkp.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-&(hp3q&)3zdvfe#ur3wmabwse8)d!*b=qrlcousek6jdve08s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
